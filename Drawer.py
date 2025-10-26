b5ef867413a423d0f4f178d581011181276c0d85
Kate Flamingbutt <em1997@yandex.ru>
2025-10-26 21:48:49 +0300

    multi sizes

--- Drawer.py
+++ Drawer.py
@@ -3,6 +3,7 @@ import cv2
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import string
+from copy import deepcopy
 
 def generate_random_string(length):
     characters = string.ascii_letters + string.digits
@@ -11,6 +12,9 @@ def generate_random_string(length):
 
 class Drawer:
     def __init__(self, size=(1920,800),length=60,rate=1, field_size=(100,100)):
+        self.offset_x = None
+        self.offset_y = None
+        self.multiplier = None
         self.x,self.y=size
         self.field_x,self.field_y=field_size
         self.length = length
@@ -18,9 +22,7 @@ class Drawer:
         self.current_state=[[(0,0,0) for _ in range(self.field_x)] for _ in range(self.field_y)]
         self.time=0
         self.file_name="videos/"+generate_random_string(10)
-        self.multiplier=min(self.x/self.field_x,self.y/self.field_y)
-        self.offset_x=(self.x-self.field_x*self.multiplier)/2
-        self.offset_y = (self.y - self.field_y * self.multiplier) / 2
+        self.states=[]
 
     def next_state(self):
         self.time+=1
@@ -30,31 +32,44 @@ class Drawer:
                     (random.randint(0,255),
                      random.randint(0,255),
                      random.randint(0,255))
+        return self.current_state
 
-    def draw_image(self):
-        img = Image.new("RGB", (self.x, self.y), (255, 255, 255))
+    def get_all_states(self):
+        for cadre in range(0, self.length*self.rate):
+            self.states.append(deepcopy(self.next_state()))
+
+    def draw_image(self,state,size=None):
+        if size is None:
+            size = (self.x, self.y)
+        img = Image.new("RGB", size, (255, 255, 255))
         draw = ImageDraw.Draw(img)
+        self.multiplier = min(size[0] / self.field_x, size[1] / self.field_y)
+        self.offset_x = (size[0] - self.field_x * self.multiplier) / 2
+        self.offset_y = (size[1] - self.field_y * self.multiplier) / 2
         for i in range(0, self.field_y):
             for j in range(0, self.field_x):
                 draw.rectangle((self.offset_x+j*self.multiplier,self.offset_y+i*self.multiplier,
                                 self.offset_x+(j+1)*self.multiplier,self.offset_y+(i+1)*self.multiplier),
-                               self.current_state[i][j])
+                               state[i][j])
 
         return img
 
-    def generate_video(self):
+    def generate_video(self, sizes=None):
+        if sizes is None:
+            sizes = [(self.x, self.y)]
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')  # Or 'XVID', 'DIVX', etc.
-        out = cv2.VideoWriter(self.file_name+'.avi', fourcc, self.rate, (self.x, self.y))
-        for cadre in range(0, self.length*self.rate):
-            self.next_state()
-            img=self.draw_image()
-            print("LOG: Working on {0}th image".format(cadre))
-            frame_rgb = np.array(img)
-            # Convert RGB to BGR (OpenCV's default color order)
-            frame_bgr = cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2BGR)
-            out.write(frame_bgr)
-        cv2.destroyAllWindows()
-        out.release()
+        self.get_all_states()
+        for size in sizes:
+            out = cv2.VideoWriter(self.file_name + str(size[0])+'_'+str(size[1])+'.avi', fourcc, self.rate, (size[0], size[1]))
+            for cadre in self.states:
+                img=self.draw_image(cadre,size)
+                print("LOG: Plus image")
+                frame_rgb = np.array(img)
+                # Convert RGB to BGR (OpenCV's default color order)
+                frame_bgr = cv2.cvtColor(frame_rgb, cv2.COLOR_RGB2BGR)
+                out.write(frame_bgr)
+            cv2.destroyAllWindows()
+            out.release()
 
 
 
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ width=800
 
 drawer = Drawer.Drawer((600,1000),5,5, (5,5))
 
-drawer.generate_video()
+drawer.generate_video([(600,1000),(100,100),(1000,400)])
